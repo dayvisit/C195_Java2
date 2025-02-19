f6c93e80cba061655f10a751a2fbaddcd779cd31
Benjamin Davis <davisb2010@gmail.com>
2025-02-19 13:45:44 -0500

    controllers/models

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,11 @@ module davis.c {
     requires java.sql;
     requires mysql.connector.j;
 
-
     opens davis.c195 to javafx.fxml;
-    exports davis.c195;
-    exports davis.c195.controller;
     opens davis.c195.controller to javafx.fxml;
     opens davis.c195.model to javafx.base;
+
+    exports davis.c195;
+    exports davis.c195.controller;
+    exports davis.c195.model;
 }
